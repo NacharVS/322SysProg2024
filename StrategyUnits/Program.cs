6d8de033f0fa31c6a9f5bbaf5c6cbee7a791b30d
muxomor <7baltika9@gmail.com>
2024-01-12 13:32:59 +0300

    bishop energy

--- StrategyUnits/Program.cs
+++ StrategyUnits/Program.cs
@@ -13,3 +13,4 @@ ps1.ShowInfo();
 footman2.ShowInfo();
 bishop1.HealUnit(footman2);
 footman2.ShowInfo();
+bishop1.ShowInfo();
--- StrategyUnits/Unit.cs
+++ StrategyUnits/Unit.cs
@@ -28,9 +28,9 @@
             Console.WriteLine("Is moving");
         }
 
-        public void ShowInfo()
+        public virtual void ShowInfo(string text = "")
         {
-            Console.WriteLine($"Unit: {_name} Health: {_health}");
+            Console.WriteLine($"Unit: {_name} Health: {_health} " + text);
         }
     }
 }
