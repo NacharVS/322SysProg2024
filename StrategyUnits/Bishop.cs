c9f6f9051a6a5801c5397bb3c7d062d34172dc99
1BlueSparkle1 <kukuruzum@mail.ru>
2024-01-12 15:12:08 +0300

    Adding MagicUnit

--- StrategyUnits/Bishop.cs
+++ StrategyUnits/Bishop.cs
@@ -9,49 +9,27 @@ namespace StrategyUnits
     internal class Bishop : Unit
     {
         private int _hill;
-        private int _energy;
-        public int MaxEnergy { get; private set; }
-
+        
         public int Hill
         {
             get { return _hill; }
             set { _hill = value; }
         }
 
-        public int Energy
-        {
-            get { return _energy; }
-            set
-            {
-                if (value <= 0)
-                {
-                    _energy = 0;
-                }
-                else if (value > MaxEnergy)
-                {
-                    _energy = MaxEnergy;
-                }
-                else
-                {
-                    _energy = value;
-                }
-            }
-        }
+        
 
         public Bishop() : base(20, "Bishop")
         {
             _hill = 1;
-            _energy = 60;
-            MaxEnergy = _energy;
         }
 
-        public void InFlickHill(Unit unit)
+        public void InFlickHill(MagicUnit magicUnit)
         {
-            while (unit.Health < unit.MaxHealth)
+            while (magicUnit.Health < magicUnit.MaxHealth)
             {
-                if (Energy <= 0) { break; }
-                unit.Health += _hill;
-                Energy -= 2;
+                if (magicUnit.Energy <= 0) { break; }
+                magicUnit.Health += _hill;
+                magicUnit.Energy -= 2;
             }
             
         }
