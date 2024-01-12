83e078b5c951a3cffe3d3d259c437ec9346b9f13
shy7331 <gart.25@mail.ru>
2024-01-12 13:18:16 +0300

    p

--- StrategyUnits/Program.cs
+++ StrategyUnits/Program.cs
@@ -10,6 +10,6 @@ footman2.ShowInfo();
 bishop.ShowInfo();
 footman.InflictDamage(ps1);
 footman.InflictDamage(footman2);
-bishop.Healinghealth(ps1);
+bishop.Healinghealth(ps1); 
 ps1.ShowInfo();
 footman2.ShowInfo();
