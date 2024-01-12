1fb214283251180488bb3a0c8ea3f024f4d4a459
DegasiPilot <Timurnig@gmail.com>
2024-01-12 13:14:26 +0300

    Create Bishop

---
+++ StrategyUnits/Bishop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyUnits
+{
+    internal class Bishop : Unit
+    {
+        private int _healAmount;
+        private int _energy;
+
+        public int Energy
+        {
+            get { return _energy; }
+            set { _energy = value; }
+        }
+
+        public int HealAmount
+        {
+            get { return _healAmount; }
+            set { _healAmount = value; }
+        }
+
+        public Bishop() : base(50, "Bishop")
+        {
+            _healAmount = 5;
+        }
+
+        public void Heal(Unit unit)
+        {
+            unit.Health += _healAmount;
+        }
+    }
+}
--- StrategyUnits/Program.cs
+++ StrategyUnits/Program.cs
@@ -3,10 +3,13 @@
 Footman footman = new Footman();
 Footman footman2 = new Footman();
 Peasant ps1 = new Peasant();
+Bishop bishop1 = new Bishop();
 
 ps1.ShowInfo();
 footman2.ShowInfo();
 footman.InflictDamage(ps1);
 footman.InflictDamage(footman2);
 ps1.ShowInfo();
+footman2.ShowInfo();
+bishop1.Heal(footman2);
 footman2.ShowInfo();
