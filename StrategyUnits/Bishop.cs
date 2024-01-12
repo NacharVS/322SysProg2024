9475daf214df7ea527f569d4be0800aaa4ec9a56
muxomor <7baltika9@gmail.com>
2024-01-12 13:00:50 +0300

    bishop

---
+++ StrategyUnits/Bishop.cs
@@ -0,0 +1,20 @@
+﻿namespace StrategyUnits
+{
+    internal class Bishop : Unit
+    {
+        private int _heal;
+        public int Heal
+        {
+            get { return _heal; }
+            set { _heal = value; }
+        }
+        public Bishop() : base(60, "Bishop")
+        {
+            _heal= 7;
+        }
+        public void HealUnit(Unit unit)
+        {
+            unit.Health += _heal;
+        }
+    }
+}
