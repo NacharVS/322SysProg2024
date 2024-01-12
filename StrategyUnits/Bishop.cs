d50e81dc7b5c502a5b33da01e2417c8f6a6d9bf9
shy7331 <gart.25@mail.ru>
2024-01-12 13:17:30 +0300

    add bishop

---
+++ StrategyUnits/Bishop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyUnits
+{
+    internal class Bishop : Unit
+    {
+
+        private int _healing;
+
+        public int healing
+        {
+            get { return _healing; }
+            set { _healing = value; }
+        }
+        public Bishop() : base(45, "Peasant")
+        {
+            _healing = 15;
+        }
+        public void Healinghealth(Unit unit)
+        {
+            unit.Health += _healing;
+        }
+
+
+    }
+}
--- StrategyUnits/Program.cs
+++ StrategyUnits/Program.cs
@@ -3,10 +3,13 @@
 Footman footman = new Footman();
 Footman footman2 = new Footman();
 Peasant ps1 = new Peasant();
+Bishop bishop = new Bishop();
 
 ps1.ShowInfo();
 footman2.ShowInfo();
+bishop.ShowInfo();
 footman.InflictDamage(ps1);
 footman.InflictDamage(footman2);
+bishop.Healinghealth(ps1);
 ps1.ShowInfo();
 footman2.ShowInfo();
