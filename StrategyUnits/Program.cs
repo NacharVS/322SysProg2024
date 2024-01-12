858dd19488e53e74121a2cb4c40150442b287f2e
222111 <222111@1220-010-22>
2024-01-12 13:14:45 +0300

    first

--- StrategyUnits/Program.cs
+++ StrategyUnits/Program.cs
@@ -3,6 +3,7 @@
 Footman footman = new Footman();
 Footman footman2 = new Footman();
 Peasant ps1 = new Peasant();
+Bishop bishop1 = new Bishop();
 
 ps1.ShowInfo();
 footman2.ShowInfo();
@@ -10,3 +11,7 @@ footman.InflictDamage(ps1);
 footman.InflictDamage(footman2);
 ps1.ShowInfo();
 footman2.ShowInfo();
+bishop1.InflictHeal(footman2);
+footman2.ShowInfo();
+bishop1.InflictHeal(ps1);
+ps1.ShowInfo();
