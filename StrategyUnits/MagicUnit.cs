40a8df7812df5d09f3304593088abae2f587224f
muxomor <7baltika9@gmail.com>
2024-01-12 15:07:56 +0300

    Baikalian Fountain upd

---
+++ StrategyUnits/MagicUnit.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyUnits
+{
+    internal class MagicUnit : Unit
+    {
+        public int _energy;
+        public int Energy
+        {
+            get
+            {
+                return _energy;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _energy = 0;
+                }
+                else
+                {
+                    if (value > MaxHealth)
+                    {
+                        _energy = MaxHealth;
+                    }
+                    else
+                    {
+                        _energy = value;
+                    }
+                }
+            }
+        }
+        public int RemovedEnergy
+        {
+            get => MaxEnergy - Energy;
+        }
+
+        public int MaxEnergy;
+        public MagicUnit(int health, string? name) : base(health, name)
+        {
+        }
+    }
+}
