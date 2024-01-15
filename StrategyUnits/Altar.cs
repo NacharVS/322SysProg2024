c88060fed8b5bb8692b288cc3b238b992e95efcb
milen <milen@DESKTOP-142GG8M>
2024-01-15 09:42:46 +0300

    dz

--- StrategyUnits/Altar.cs
+++ StrategyUnits/Altar.cs
@@ -8,6 +8,26 @@ namespace StrategyUnits
 {
     internal class Altar
     {
-        private int altar;
+        private int _reserv;
+        public int Reservs
+        {
+            get { return _reserv; }
+            set { _reserv = value; }
+        }
+        public Altar()
+        {
+            _reserv = 250;
+        }
+        public void BishopAltar(Bishop bishop)
+        {
+            while (Reservs > 0 && bishop.energy < 60)
+            {
+
+                bishop.energy++;
+                Reservs--;
+            }
+
+
+        }
     }
 }
--- StrategyUnits/Program.cs
+++ StrategyUnits/Program.cs
@@ -7,14 +7,20 @@ Bishop bishop1 = new Bishop();
 Bishop bishop2 = new Bishop();
 
 ps1.ShowInfo();
-//footman2.ShowInfo();
+footman2.ShowInfo();
 footman.InflictDamage(ps1);
-//footman.InflictDamage(footman2);
-//footman.InflictDamage(footman2);
+footman.InflictDamage(footman2);
+footman.InflictDamage(footman2);
 ps1.ShowInfo();
-//footman2.ShowInfo();
+footman2.ShowInfo();
 bishop1.InflictHeal(ps1);
-//bishop2.InflictHeal(footman2);
+bishop2.InflictHeal(footman2);
 ps1.ShowInfo();
-//footman2.ShowInfo();
-//bishop1.ShowInfo();
+footman2.ShowInfo()
+bishop1.ShowInfo();
+Altar altar = new Altar();
+bishop1.InflictHeal(footman2);
+bishop1.ShowInfo();
+altar.BishopAltar(bishop1);
+bishop1.ShowInfo();
+//
