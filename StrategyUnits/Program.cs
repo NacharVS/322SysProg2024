0a103cebc5d57332e76b8ec65d0e94e0314863a4
Зарина Габнасирова <gabnasirovalove@mail.ru>
2024-01-15 12:22:58 +0300

    added an altar

--- StrategyUnits/Program.cs
+++ StrategyUnits/Program.cs
@@ -3,10 +3,21 @@
 Footman footman = new Footman();
 Footman footman2 = new Footman();
 Peasant ps1 = new Peasant();
+Bishop bishop = new Bishop();
 
 ps1.ShowInfo();
-footman2.ShowInfo();
+footman.ShowInfo();
+bishop.ShowInfo();
+bishop.InfoEnergy();
+Console.WriteLine();
+
 footman.InflictDamage(ps1);
-footman.InflictDamage(footman2);
+footman.InflictDamage(ps1);
+footman.InflictDamage(ps1);
+//ps1.Health += 100;
+ps1.ShowInfo();
+Console.WriteLine();
+
+bishop.Treatment(ps1);
 ps1.ShowInfo();
 footman2.ShowInfo();
