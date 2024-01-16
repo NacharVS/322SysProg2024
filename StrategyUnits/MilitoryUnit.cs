bb595e40bfc5eb37b63bb33d6ab050e0afed4007
Лера <kukuruzum@mail.ru>
2024-01-16 22:54:25 +0300

    The ilitary unit class was created

---
+++ StrategyUnits/MilitoryUnit.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyUnits
+{
+    internal class MilitoryUnit : Unit
+    {
+        private int _damage;
+        public int Damage
+        {
+            get { return _damage; }
+            set { _damage = value; }
+        }
+        private int _defence;
+        public int Defence
+        {
+            get { return _defence; }
+            set { _defence = value; }
+        }
+        public int _block;
+        public virtual int Block
+        {
+            get { return _block; }
+            set
+            {
+                if (Damage > Defence)
+                {
+                    _block = Damage - Defence;
+                }
+                else
+                {
+                    _block = 0;
+                }
+            }
+        }
+        public MilitoryUnit(int health, string? name) : base(health, name)
+        {
+
+        }
+    }
+}
