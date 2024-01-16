b6cd4a4caf5101d161b1c691806f7e407263f22c
SKTDLAR <burtovdanil2005@gmail.com>
2024-01-16 18:37:21 +0300

    Added Palladin with his ultimate "SaintSword", added intermediate type of units "MillitaryUnit"

--- StrategyUnits/Program.cs
+++ StrategyUnits/Program.cs
@@ -4,6 +4,7 @@ Footman footman = new Footman();
 Peasant ps1 = new Peasant();
 
 ps1.ShowInfo();
+footman2.ShowInfo();
 footman.InflictDamage(ps1);
 footman.InflictDamage(ps1);
 footman.InflictDamage(ps1);
