d161cd4eb883fd6d525c2b5e0952e52b822b2ea8
Viniche <156420232+Viniche@users.noreply.github.com>
2024-01-17 18:30:28 +0300

    altar

---
+++ StrategyUnits/Altar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyUnits
+{
+    internal class Altar
+    {
+        private int _increase;
+        public Altar() { _increase = 10; }
+        public int Increase
+        {
+            get 
+            { 
+                return _increase; 
+            }
+
+            set 
+            { 
+                _increase = value; 
+            }
+        }
+        public void GetIncrease(Bishop bishop1)
+        {
+            bishop1.Energy += _increase;
+        }
+    }
+}
+
--- StrategyUnits/Program.cs
+++ StrategyUnits/Program.cs
@@ -4,10 +4,13 @@ Footman footman = new Footman();
 Footman footman2 = new Footman();
 Peasant ps1 = new Peasant();
 Bishop bishop1 = new Bishop();  
-
+Altar altar1 = new Altar();
 ps1.ShowInfo();
 footman.InflictDamage(ps1);
 ps1.ShowInfo();
 bishop1.InflictHeal(ps1,bishop1);
 ps1.ShowInfo();
 bishop1.ShowInfoBishop();
+altar1.GetIncrease(bishop1);
+bishop1.ShowInfoBishop();
+ 
