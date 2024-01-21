92ede79c3521484bf7c6c6503e08503bd388da08
Лера <kukuruzum@mail.ru>
2024-01-22 02:49:55 +0300

    The Berserker character has been added and Frenzy attack has been made for him

---
+++ StrategyUnits/Berserker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyUnits
+{
+    internal class Berserker : Footman
+    {
+        public void Frenzy(Unit unit)
+        {
+            int damage = RandomDamage;
+            if (Dead)
+            {
+                Console.WriteLine("Атака не может быть проведена - атакующий персонаж мертв");
+            }
+            else if (unit.Dead)
+            {
+                Console.WriteLine("Атака не может быть проведена - персонаж, которого атакуют, мертв");
+            }
+            else
+            {
+                if (Health < (double)MaxHealth/100*30)
+                {
+                    unit.TakingDamage(damage + damage*5);
+                }
+                else
+                {
+                    unit.TakingDamage(damage);
+                }
+
+            }
+        }
+    }
+}
