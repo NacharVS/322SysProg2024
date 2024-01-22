d57fc000434aa429e09f14a4b9f778dfa61c8fee
Лера <kukuruzum@mail.ru>
2024-01-22 03:13:52 +0300

    The Zealot character has been added and the ArmorOfFaith ability has been created for him

---
+++ StrategyUnits/Zealot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyUnits
+{
+    internal class Zealot : Paladin
+    {
+        public bool CheackArmor = false;
+        public void ArmorOfFaith()
+        {
+            if (Dead)
+            {
+                Console.WriteLine("Невозможно выполнить это действие, персонаж мертв");
+            }
+            else
+            {
+                if (Health <= (double)MaxHealth / 100 * 30 && !CheackArmor)
+                {
+                    CheackArmor = true;
+                    Defence += Defence;
+                }
+                else if (Health > (double)MaxHealth / 100 * 30 && CheackArmor)
+                {
+                    Defence -= Defence;
+                    CheackArmor = false;
+                }
+            }
+        }
+    }
+}
