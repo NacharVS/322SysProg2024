abf6dfeda7486d22704771dd4d43e58c466d5bc1
SKTDLAR <burtovdanil2005@gmail.com>
2024-01-26 14:41:59 +0300

    злоцуыщатщцукращжгиуц

--- StrategyUnits/Garbage/MilitaryUnit.cs
+++ StrategyUnits/Garbage/MilitaryUnit.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,3 +27,4 @@ namespace StrategyUnits.Garbage
         }
     }
 }
+*/
