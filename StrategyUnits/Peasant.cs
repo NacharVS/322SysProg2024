e3d158c1bd28a8d5bb1924b8e0af134f73bb72be
SKTDLAR <burtovdanil2005@gmail.com>
2024-01-26 14:43:24 +0300

    фвыафуав

--- StrategyUnits/Peasant.cs
+++ StrategyUnits/Peasant.cs
@@ -1,26 +1,18 @@
 ﻿namespace StrategyUnits
 {
-    internal class Peasant : Unit, IHealthControl, IArmoredUnit
+    internal class Peasant : Unit, IHealthControl
     {
         public Peasant() : base(30, "Peasant")
         {
             
         }
 
-        public int Armor { get; set; }
 
         public void TakeDamage(double Damage)
         {
-            if (Armor > Damage)
-            {
-                Armor -= (int)Damage;
-            }
-            else
-            {
-                Damage -= Armor;
-                Armor = 0;
+            
                 health -= Damage;
-            }
+            
         }
     }
 }
