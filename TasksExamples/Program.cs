6ddb8ff95ef86c2790d745b8331f856ef1880896
r3dinc <r3dinc@yandex.ru>
2024-02-12 12:15:09 +0300

    tasks start lesson

---
+++ TasksExamples/Program.cs
@@ -0,0 +1,22 @@
+﻿Task task = new Task(() =>
+{
+    Console.WriteLine("Task Started");
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"task: {i}");
+        Thread.Sleep(500);
+    }
+    Console.WriteLine("Task Ended");
+});
+task.Start();
+
+
+Console.WriteLine("Main started");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"main: {i}");
+    Thread.Sleep(500);
+}
+task.Wait();
+Console.WriteLine("Main ended");
