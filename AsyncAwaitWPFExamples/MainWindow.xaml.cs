033037fd46590e3641f0e9206b8d62efc3271c4b
r3dinc <r3dinc@yandex.ru>
2024-02-26 12:10:29 +0300

    async await wpf implementation part1

---
+++ AsyncAwaitWPFExamples/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncAwaitWPFExamples
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            while (File.Exists(@"C:\Users\Vadim.Nacharov\Desktop\testdoc.txt"))
+            {
+
+            }
+            MessageBox.Show("File is deleted or changed!");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            lbl1.Content = "qqqqqqqqqqqqq";
+        }
+    }
+}
